297ad3ecfc12cb4c34b18a96b7532c8eb335fd85
danil <danildjohan12@gmail.com>
2021-07-31 14:18:22 -0600

    add: Login system

---
+++ WebApp/src/main/java/com/github/innovaccionvirtual/web/HomeController.java
@@ -0,0 +1,32 @@
+package com.github.innovaccionvirtual.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @Autowired
+    PasswordEncoder encoder;
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
+}
