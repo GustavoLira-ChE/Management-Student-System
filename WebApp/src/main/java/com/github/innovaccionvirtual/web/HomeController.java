b2594a4e86d9653c3e15ea64b1e802ca6e7c7dc6
danil <danildjohan12@gmail.com>
2021-07-31 17:48:45 -0600

    add: Second Attempt to create login

--- WebApp/src/main/java/com/github/innovaccionvirtual/web/HomeController.java
+++ WebApp/src/main/java/com/github/innovaccionvirtual/web/HomeController.java
@@ -1,9 +1,13 @@
 package com.github.innovaccionvirtual.web;
 
+import com.github.innovaccionvirtual.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -21,9 +25,19 @@ public class HomeController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@ModelAttribute User user) {
         return "login";
     }
+
+    @GetMapping("login-error")
+    public ModelAndView login() {
+        return new ModelAndView("login", "error", true);
+    }
+
+    /*@GetMapping("/login")
+    public String login(@ModelAttribute User user) {
+        return "index";
+    }*/
 
     @GetMapping("/access-denied")
     public String accessDenied() {
